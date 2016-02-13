9ec7d20dfe98a2d6c1604d365d1cb2ee0c952355
andydlin <andydlin@gmail.com>
2016-02-13 00:27:21 -0800

    adf

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    var mainInnerHeight = $('.main-inner .inner-content').outerHeight();
+    var marginTop = (mainInnerHeight / 2) * -1;
+    $('.main-inner').css({
+        'height' : mainInnerHeight,
+        'margin-top' : marginTop
+    });
+    
+    setTimeout(function() {
+        $('body.home .main').addClass('active');
+    }, 1000);
+
+    checkOptionsHeight();
+});
+
+$(window).resize(function() {
+    checkOptionsHeight();
+});
+
+function checkOptionsHeight() {
+    var fsHeight = $('.for-students').outerHeight();
+    var feHeight = $('.for-employers').outerHeight();
+    if(fsHeight > feHeight) {
+        $('.for-employers').css('height', fsHeight);
+    } else if(fsHeight < feHeight) {
+        $('.for-students').css('height', feHeight);
+    } else {
+        $('.for-employers, .for-students').css('height', '');
+    }
+}
